9af3f516fa29a8af3c63af1ccbd5e8a8dbbd9586
nuruzzamanhimel00 <nuruzzaman.himel147@gmail.com>
2023-09-01 17:05:15 +0600

    done

--- src/components/CompositionApi.vue
+++ src/components/CompositionApi.vue
@@ -1,24 +1,36 @@
 <template>
   <div class="text-center">
     <h1>My name is: {{ userName }}</h1>
+    <h1>Usser Details</h1>
+    <p>Name: {{ userDetails.name }}</p>
+    <p>Name: {{ userDetails.age }}</p>
   </div>
 </template>
 
 <script>
-import { ref } from "vue";
+import { reactive, ref } from "vue";
 
 export default {
   setup() {
     let userName = ref("My name is Himel");
 
+    let userDetails = reactive({
+      name: " Himel",
+      age: 29,
+    });
+
     setTimeout(() => {
       console.log("set timeout is called");
-      console.log(userName);
+      console.log(userName, userDetails);
       userName.value = "My name is Leela";
+
+      userDetails.name = "Update Himel";
+      userDetails.age = "Update 20";
     }, 2000);
 
     return {
       userName: userName,
+      userDetails,
     };
   },
 };
