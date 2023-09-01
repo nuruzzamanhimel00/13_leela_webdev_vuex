6adb07270c1581ab2561d58e04da78f066d66a2a
nuruzzamanhimel00 <nuruzzaman.himel147@gmail.com>
2023-09-01 16:22:49 +0600

    done

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,17 @@
     <counter-component></counter-component>
     <todo-list></todo-list>
     <p class="text-center">Done Tod list: {{ doneToDoListsCount }}</p>
+    <CompositionApi />
   </div>
 </template>
 
 <script>
 import CounterComponent from "./components/CounterComponent.vue";
 import TodoList from "./components/TodoList.vue";
+import CompositionApi from "./components/CompositionApi.vue";
 export default {
   name: "App",
-  components: { CounterComponent, TodoList },
+  components: { CounterComponent, TodoList, CompositionApi },
   computed: {
     doneToDoListsCount() {
       return this.$store.getters.doneTodosCount;
---
+++ src/components/CompositionApi.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="text-center">
+    <h1>My name is: {{ userName }}</h1>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  setup() {
+    let userName = ref("My name is Himel");
+
+    setTimeout(() => {
+      console.log("set timeout is called");
+      console.log(userName);
+      userName.value = "My name is Leela";
+    }, 2000);
+
+    return {
+      userName: userName,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
